b9096855e11ae3faf27fa3b4f57d76eed63854f9
Shindi07 <shindicahyandari8@gmail.com>
2024-02-25 15:15:17 +0700

    first commit

---
+++ src/com/program/Konfig.java
@@ -0,0 +1,24 @@
+
+package com.program;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author windows
+ */
+public class Konfig {
+    private static Connection mysqlconfig;
+    public static Connection configDB()throws SQLException{
+        try {
+            String url="jdbc:mysql://localhost:3306/database_crud"; //url database
+            String user="root"; //user database
+            String pass=""; //password database
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            mysqlconfig=DriverManager.getConnection(url, user, pass);            
+        } catch (Exception e) {
+            System.err.println("koneksi gagal "+e.getMessage()); //perintah menampilkan error pada koneksi
+        }
+        return mysqlconfig;
+    }    
+}
